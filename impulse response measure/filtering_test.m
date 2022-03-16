11b78b13c41e5b26d2ea1c48990a41a51f286034
Heo Junyeong <89557425+Heo-Junyeong@users.noreply.github.com>
2022-03-16 15:56:43 +0900

    Add files via upload

--- impulse response measure/filtering_test.m
+++ impulse response measure/filtering_test.m
@@ -1,10 +1,9 @@
 clear;
 
 clean_sp = audioread('FDHC0_SI929.wav');
-path = audioread('ir_2.wav');
-path = path(1:744);
-##path = audioread('Impulse_Response_1.wav');
+path = audioread('p_ir.wav');
 
 echo = filter(path, 1, clean_sp);
 figure, plot(clean_sp);
 hold on, plot(echo);
+sound(echo, 16000);
--- impulse response measure/find_ir_delay.m
+++ impulse response measure/find_ir_delay.m
@@ -1,10 +1,11 @@
 pkg load signal;
+close all;
+
 [ir1, fs] = audioread('p_ir.wav');
 [ir2, fs] = audioread('s_ir.wav');
 
-ir1 = resample(ir1,16000,44100);
-ir2 = resample(ir2,16000,44100);
-
+ir1 = resample(ir1,16000,64000);
+ir2 = resample(ir2,16000,64000);
 
 figure; hold on; grid on; grid minor;
 plot(ir1, 'k-'), xlabel('samples'), ylabel('values');
@@ -12,20 +13,34 @@ plot(ir2, 'r-');
 title('path impulse response');
 xlim([1, 400]);
 
+sr = 4000;
 
-signal1 = ir1(190:254);
-signal2 = ir2(100:164);
+% Parameters
+pure_delay_sir = 70; pure_delay_pir = 142;
+sir_tap = 16; pir_tap = 16;
 
+% save directory
+root_dir = './ir_results/';
 
-figure; plot(signal1, 'b-'), xlabel('samples'), ylabel('values');
+% Routine
+s_cut = pure_delay_sir : pure_delay_sir + sir_tap;
+e_cut = pure_delay_pir : pure_delay_pir + pir_tap;
+
+signal1 = ir1(e_cut);
+signal2 = ir2(s_cut);
+
+figure; plot(signal1, 'k-'), xlabel('samples'), ylabel('values');
 title('path impulse response(no delay)');
 
 hold on;
 plot(signal2, 'r-');
 legend('p ir','s ir');
 
-xlim([1, 64]); ylim([-0.03 0.06]);
+xlim([1, pir_tap]);
 
 hold on;
 grid on;
 grid minor;
+
+audiowrite(strcat(root_dir, 'p_ir_nd_tap', num2str(pir_tap),'_sr' , num2str(sr), '_puredelay', num2str(pure_delay_pir), '.wav'), ir1, 4000);
+audiowrite(strcat(root_dir, 's_ir_nd_tap', num2str(sir_tap),'_sr' , num2str(sr), '_puredelay', num2str(pure_delay_sir), '.wav'), ir2, 4000);
